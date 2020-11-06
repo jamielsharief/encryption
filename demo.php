0d91c019935de77eb8e50936be87010872938335
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2020-11-06 07:50:46 +0100

    Added demo

---
+++ demo.php
@@ -0,0 +1,27 @@
+<?php
+
+use Encryption\KeyChain;
+use Encryption\AsymmetricEncryption;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$path = __DIR__ . '/demo';
+@mkdir($path);
+$keyChain = new KeyChain($path);
+
+$keyChain->create('demo@example.com');
+
+echo "\nKeys in key chain:\n";
+print_r($keyChain->list());
+
+echo "\nKey generated:\n";
+$document = $keyChain->get('demo@example.com');
+print_r($document);
+
+echo "\nEncrypted text:\n";
+$crypto = new AsymmetricEncryption();
+$encrypted = $crypto->encrypt('foo', $document['publicKey']);
+echo $encrypted . PHP_EOL;
+
+echo "\nDecrypted text:\n";
+echo $crypto->decrypt($encrypted, $document['privateKey']);
